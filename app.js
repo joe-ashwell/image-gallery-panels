043a425d6a38389f67edecdfbf40c0b6d500ac61
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2020-09-27 19:05:57 +0100

    Include smoother animation

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-const panel = document.querySelectorAll('.panel');
+const panels = document.querySelectorAll('.panel');
 
-panel.forEach(item => {
+panels.forEach(item => item.addEventListener('mouseover', togglePanelOpen));
+panels.forEach(item => item.addEventListener('transitionend', togglePanelActive));
 
-  item.addEventListener('mouseover', () => {
+panels.forEach(item => item.addEventListener('mouseover', {
 
-    item.classList.add('open-panel');
+}));
 
-  })
+panels.forEach(item => {
+  item.addEventListener('mouseover', togglePanelOpen);
+  item.addEventListener('mouseout', togglePanelClose);
+})
 
-  item.addEventListener('mouseout', () => {
+function togglePanelOpen() {
+  this.classList.add('open-panel');
+}
 
-    item.classList.remove('open-panel');
+function togglePanelClose() {
+  this.classList.remove('open-panel');
+}
 
-  })
-
-}) 
+function togglePanelActive(e) {
+  if(e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
